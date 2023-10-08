8c582e05c63e497c9b3c122fe6f5d00d170f8e26
568j4 <144990819+568j4@users.noreply.github.com>
2023-10-08 19:05:41 +0900

    3

--- main.c
+++ main.c
@@ -3,30 +3,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[])
 
- {
- 	
-    int number;
+int main() {
+    char c;
+    int num = 0;
 
+    printf("input a string: ");
 
-    printf("Input an integer: ");
-    scanf("%d", &number);
-
-
-    if (number < 0) {
-        number = -number;
+    while ((c = getchar()) != '\n') {
+        if (c >= '0' && c <= '9') {
+            num++;
+        }
     }
 
-
-    printf("The absoulte value is %d\n", number);
+    printf("The number of digits is: %d\n", num);
 
     return 0;
 }
-
-
-
-
-
-
-
